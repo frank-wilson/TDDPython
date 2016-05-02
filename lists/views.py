0faaa29d48aa1dcb6394bfbfd51d9aa77887914c
Frank <franciscolhernan@gmail.com>
2016-05-02 12:58:08 -0700

    Added FT to test for multiple users

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
-# TODO: Support more than one list.
+# TODO: Adjust model so that items are associated with different lists
+# TODO: Add unique URLs for each list
+# TODO: Add a URL for creating a new list via POST
+# TODO: Add URLs for adding a new item to an existing list via POST
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
